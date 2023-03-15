b6c254f970fadefbd9aa1f8d131b80dc2f75c412
MK3nn3dy <kennedy4011@outlook.com>
2023-03-15 11:36:30 +0000

    Added fetch to local backend using base from state, added useEffect for rates panel

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ require('dotenv').config();
 // imports
 const express = require('express');
 const ratesRoute = require('./routes/exchangeRates');
+const cors = require('cors');
 
 
 // create app
 const app = express();
 
 // middleware
+// cross-origin middleware for development
+app.use(cors());
 // log request details during development - can be removed in production - MK
 app.use((req, res, next) => {
     console.log(`The request path: ${req.path}`);
